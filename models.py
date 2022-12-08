d9f620ad2442717681bb23ff66a3db6fbb61744c
unknown <aabdenba@gmail.com>
2022-12-08 13:30:19 -0500

    first commit

--- models.py
+++ models.py
@@ -44,10 +44,16 @@ class NearEarthObject:
         # You should coerce these values to their appropriate data type and
         # handle any edge cases, such as a empty name being represented by `None`
         # and a missing diameter being represented by `float('nan')`.
-        self.designation = ''
-        self.name = None
-        self.diameter = float('nan')
+        self.designation = info['des']
+        if(info['diameter'] == ' '):
+            self.diameter = 0.0
+        else:
+            self.diameter = info['diameter']
         self.hazardous = False
+        if info['name'] == ' ':
+            self.name = 'None'
+        else:    
+            self.name = info['name']
 
         # Create an empty initial collection of linked approaches.
         self.approaches = []
@@ -56,14 +62,15 @@ class NearEarthObject:
     def fullname(self):
         """Return a representation of the full name of this NEO."""
         # TODO: Use self.designation and self.name to build a fullname for this object.
-        return ''
+        return '{} ({})'.format(self.designation, self.name) 
 
     def __str__(self):
         """Return `str(self)`."""
         # TODO: Use this object's attributes to return a human-readable string representation.
         # The project instructions include one possibility. Peek at the __repr__
         # method for examples of advanced string formatting.
-        return f"A NearEarthObject ..."
+        # return f"A NearEarthObject ..."
+        return "NEO {fullname} has a diameter of {diameter:.3f} km and [is/is not] potentially hazardous."
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
